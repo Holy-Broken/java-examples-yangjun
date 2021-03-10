a18cf89ea0b21782542e86bf16a6cdf0765d5219
Holy-Broken <2054803682@qq.com>
2021-03-10 20:24:13 +0800

    First of my Maven git

---
+++ src/main/java/org/examples/HelloWorld.java
@@ -0,0 +1,10 @@
+package org.examples;
+
+import java.util.List;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+        List.of();
+    }
+}
